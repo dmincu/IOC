6e681fec44be8f3b73a47bc6620b0f6b830d5c74
Diana Mincu <diana.mincu@cti.pub.ro>
2013-12-14 21:59:02 +0200

    Adds friends to database

--- new_php/html/addfriend.php
+++ new_php/html/addfriend.php
@@ -3,18 +3,39 @@
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$user = $_GET['username'];
+$myusername=$_GET['username'];
+$myfriend=$_GET['myfriend'];
+$myfriendname=$_GET['myfriendname'];
 
 $host="localhost"; // Host name
 $username="root"; // Mysql username
-$password=""; // Mysql password
+$password="Paracocci23#"; // Mysql password
 $db_name="joyinfood"; 
-$tbl_name="users"; // Table name 
+$tbl_name="friends"; // Table name 
 
 // Connect to server and select databse.
 mysql_connect("$host", "$username", "$password") or die("cannot connect");
 mysql_select_db("$db_name") or die("cannot select DB");
 
-echo $user
+// To protect MySQL injection (more detail about MySQL injection)
+$myusername=stripslashes($myusername);
+$myusername=mysql_real_escape_string($myusername);
+$myfriend=stripslashes($myfriend);
+$myfriend=mysql_real_escape_string($myfriend);
+$myfriendname=stripslashes($myfriendname);
+$myfriendname=mysql_real_escape_string($myfriendname);
+
+$sql="INSERT INTO $tbl_name (username_user, username_friend, name) VALUES (\"$myusername\", \"$myfriend\", \"$myfriendname\")";
+$result=mysql_query($sql);
+
+$sql="SELECT * FROM $tbl_name";
+$result=mysql_query($sql);
+
+while($row = mysql_fetch_array($result)) {
+  echo $row['username_user'] . " " . $row['username_friend'];
+  echo "<br>";
+}
+
+echo $result;
 
 ?>
--- new_php/joyinfood.sql
+++ new_php/joyinfood.sql
@@ -37,6 +37,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
+CREATE TABLE IF NOT EXISTS `friends` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username_user` varchar(30) NOT NULL,
+  `username_friend` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
 --
 -- Salvarea datelor din tabel `users`
 --
